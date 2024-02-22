34dc33ac06d06a7d65962b24e29b2bb85ad68219
ANKIT <kumarankit842384@gmail.com>
2024-02-22 20:01:19 +0530

    Dictionary & Contact & Game1

---
+++ contacts.py
@@ -0,0 +1,24 @@
+contacts = {
+    'police': '112',
+    'ambulance': '102',
+}
+while True:
+    print('🔍 Search a contact:')
+    q = input(">>>")
+    if len(q) == 0:
+        print('Closing the down...')
+        break
+    if q in contacts:
+        print(f'🎉(q): {contacts[q]}')
+    elif q == 'all':
+        print('All contacts:')
+        for k, v in contacts.items():
+            print(f'{k}:{v}')
+    else:
+        print(f"😭{q} not found")
+        print('📔Add a new contact?')
+        new_contact = input(">>> ")
+        if len(new_contact) == 0:
+            print('Skipping...')
+        contacts[q] = new_contact
+        print(f'{q}: {contacts[q]} added')
