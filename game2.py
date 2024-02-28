082e11b3f59146289cbd8cd57db26fe07ba68378
ANKIT <kumarankit842384@gmail.com>
2024-02-28 19:51:12 +0530

    game2&images

--- game2.py
+++ game2.py
@@ -1,8 +1,12 @@
 import random
 import pgzrun
 
+HEIGHT = 500
+WIDTH = 800
+
 class Hero(Actor):
-    def move(self, dx=3, dy=0):
+    def move(self):
+        
         if keyboard.left:
             self.x -= 5
         elif keyboard.right:
@@ -12,15 +16,48 @@ class Hero(Actor):
         elif keyboard.down:
             self.y += 5
 
+    def warp(self):
+        if self.x > WIDTH:
+            self.x = 0
+        if self.x < 0:
+            self.x = WIDTH
+        if self.y > HEIGHT:
+            self.y = 0
+        if self.y < 0:
+            self.y = HEIGHT
+
+        
+        
+class ship(Actor):
+    def track(self, player, speed=1):
+        if player.x > self.x:
+            self.x += speed
+        elif player.x < self.x:
+            self.x -= speed
+        if player.y > self.y:
+            self.y += speed
+        elif player.y < self.y:
+            self.y -= speed
+        
+
 HEIGHT = 500
 WIDTH = 800
 p = Hero('naruto', (100, 100))
 b = Rect((300, 100), (50, 50))  # position, size
+s = ship('pain', (1000, 1000))
+e = ship('pain', (300, 1000))
+
 def draw():
-    screen.fill('white')
+    screen.fill('black')
     p.draw()
+    s.draw()
+    e.draw()
+    p.wrap()
     screen.draw.filled_rect(b, 'blue')
 def update():
-    p.move(dx=3)
+    p.move()
+    p.wrap()
+    s.track(p,2)
+    e.track(p,1)
 
 pgzrun.go()
